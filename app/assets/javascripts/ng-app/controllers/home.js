1aff03ceeb20cb58b193d0a6cb01f2c24fbc7247
Leandro <leandro.costantini@gmail.com>
2015-01-12 20:08:25 +0000

    add bitcoin price and change erb to haml

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,3 +1,14 @@
-app.controller('HomeCtrl', function($scope){
-	$scope.things = ['one', 'two', 'three']
+app.controller('HomeCtrl', function($scope, $interval, BitcoinPrice){
+
+	$interval(function(){
+		$scope.fillBitcoinPrices();
+	}, 6000);
+
+	$scope.fillBitcoinPrices = function(){
+		BitcoinPrice.query().then(function(bitcoin_prices){
+			$scope.bitcoin_prices = bitcoin_prices;
+		});
+	};
+	$scope.fillBitcoinPrices();
+
 })
---
+++ app/assets/javascripts/ng-app/services/bitcoin_price.js
@@ -0,0 +1,18 @@
+app.factory('BitcoinPrice', ['$http', function($http){
+
+  function BitcoinPrice(data){
+    for(attr in data){
+      this[attr] = data[attr];
+    }
+  }
+
+  BitcoinPrice.query = function(){
+    return $http.get("https://api.coindesk.com/v1/bpi/currentprice.json").then(function(result){
+      var bitcoinprices = [];
+      bitcoinprices.push(new BitcoinPrice(result.data.bpi))
+      return bitcoinprices;
+    });
+  }
+
+  return BitcoinPrice;
+}]);
